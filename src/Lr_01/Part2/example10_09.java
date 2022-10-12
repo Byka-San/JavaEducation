e1fa0fb56202bf8ef61e0cc86dc9d69b30cb8b1a
BykaSan <beasty348@yandex.ru>
2022-10-12 15:34:18 +0500

    2

--- src/Lr_01/Part2/example10_09.java
+++ src/Lr_01/Part2/example10_09.java
@@ -6,10 +6,11 @@ public class example10_09 {
         Scanner in = new Scanner(System.in);
         System.out.print("Число:");
         int a = in.nextInt();
+        int d = a;
         int b = --a;
-        int c = ++a;
-        int sum = a + b + c;
-        double pow = Math.pow(a,c);
-        System.out.printf("Итог: \nЧисло: %s \nЧисло - 1: %s \nЧисло + 1: %s \nКвадрат суммы чисел: %s \n ", a, b, c, pow);
+        int c = 2 + a;
+        int sum = d + b + c;
+        double pow = sum*sum;
+        System.out.printf("Итог: \nЧисло: %s \nЧисло - 1: %s \nЧисло + 1: %s \nКвадрат суммы чисел: %s \n ", d, b, c, pow);
     }
 }
