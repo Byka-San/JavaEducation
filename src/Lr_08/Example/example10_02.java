7ed238c75d7925ac7faed975257394a71161ec91
BykaSan <beasty348@yandex.ru>
2022-12-23 15:52:12 +0500

    Lr 8.1

--- src/Lr_08/Example/example10_02.java
+++ src/Lr_08/Example/example10_02.java
@@ -1,43 +1,97 @@
 package Lr_08.Example;
 
-import java.io.ByteArrayInputStream;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
-
+import java.io.*;
+import java.util.Scanner;
 public class example10_02 {
-    public static void readAllByByte(InputStream in) throws IOException {
-        while (true){
-            int oneByte = in.read();
-            if (oneByte != -1){
-                System.out.print((char)oneByte);
+    public static void main(String[] args) throws Exception{
+        WritingFile writingFile = new WritingFile();
+        ReaderFile readerFile = new ReaderFile();
+    }
+}
+
+class WritingFile{
+    WritingFile () throws Exception {
+        try {
+            File file1 = new File("C:\\Users\\Администратор\\IdeaProjects\\JavaEducation\\Fail.txt");
+            file1.createNewFile();
+            if (file1.exists()) {
+                System.out.println("Успешно созданый файл Fail");
+                System.out.println("Полный путь: " + file1.getAbsolutePath());
             }
-            else {
-                System.out.println("\n"+"Конец");
-                break;
-            }
+            FileWriter fw = new FileWriter(file1);
+            fw.write("7.12,2.363,54.211,8.12,5.644\n89.3,764.1,-7.8979,243.34,4.245");
+            fw.close();
+            new ReaderFile(file1.getAbsolutePath());
+        }
+        catch (Exception e) {
+            System.out.println("ошибка" + e);
         }
     }
-    public static void main(String[] args) throws IOException {
-        try{
-            InputStream inFile = new FileInputStream("MyFile1.txt");
-            readAllByByte(inFile);
-            System.out.print("\n\n\n");
-            inFile.close();
+}
 
-            InputStream inUrl = new URL("http://google.com").openStream();
-            readAllByByte(inUrl);
-            System.out.print("\n\n\n");
-            inUrl.close();
 
-            InputStream inArray = new ByteArrayInputStream(new byte[]{2,5,7,1,9});
-            readAllByByte(inArray);
-            System.out.print("\n\n\n");
-            inArray.close();
+class ReaderFile {
+
+    ReaderFile (){}
+    ReaderFile (String file1) throws Exception{
+
+        try {
+            File file2 = new File("C:\\Users\\Администратор\\IdeaProjects\\JavaEducation\\rezyltat.txt");
+            file2.createNewFile();
+            if (file2.exists()) {
+                System.out.println("Успешно созданый файл rezyltat");
+                System.out.println("Полный путь к файлу: " + file2.getAbsolutePath());
+            }
+            String[] str1 = new String[5];
+            String[] str2 = new String[5];
+            String strNum2 ="";
+            String strNum1 ="";
+
+            String strDouble = "";
+
+            FileReader fr = new FileReader(file1);
+            Scanner scan = new Scanner(fr);
+
+            FileWriter fw = new FileWriter(file2);
+            String [] strNumDoub ;
+            double[] doub = new double[10];
+
+            int a = 1;
+            while (scan.hasNextLine()){
+                str1 = scan.nextLine().split(",");
+                a++;
+                if (a==2){
+                    str2 = scan.nextLine().split(",");
+                }
+            }
+
+            for (int p = 0; p < str2.length; p++){
+                strNum2 += str2[p] + ",";
+            }
+            for (int q = 0; q < str1.length; q++){
+                strNum1 += str1[q] + ",";
+            }
+            String strNum = strNum1  + strNum2;
+
+            strNumDoub = strNum.split(",");
+
+            for (int q = 0 ; q<strNumDoub.length;q++){
+                doub[q] = Double.parseDouble(strNumDoub[q]);
+            }
+
+            for (int q = 0; q < doub.length;q++) {
+                if (doub[q] > 0){
+                    strDouble += doub [q] + " ";
+                }
+            }
+
+            fw.write("Строка 2 содержит: " + strNum2 + "\n" + "Положительными часлами типа Double является: " + strDouble );
+            fw.close();
+            fr.close();
+
+        }catch (Exception e){
+            System.out.println("Ошибка: " + e);
         }
-        catch (IOException e) {
-            System.out.println("Произошла ошибка. Причина: "+ e);
-        }
+
     }
 }
--- src/Lr_08/Example/example10_03.java
+++ src/Lr_08/Example/example10_03.java
@@ -1,42 +1,38 @@
 package Lr_08.Example;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Arrays;
+import java.io.*;
+import java.util.*;
 
 public class example10_03 {
-    public static void readAllByArray(InputStream in) throws IOException {
-        byte[] buff = new byte[5];
-        while (true) {
-            int count = in.read(buff);
-            if (count != -1) {
-                System.out.println("Количество = " + count + ", buff = "
-                        + Arrays.toString(buff) + ", str = "
-                        + new String(buff, 0, count, "cp1251"));
-            }
-            else {
-                break;
-            }
-        }
-    }
-
-
     public static void main(String[] args) throws IOException {
-        String fileName = "MyFile1.txt";
-        InputStream inFile = null;
-        try {
-            inFile = new FileInputStream(fileName);
-            readAllByArray(inFile);
-        } catch (IOException e) {
-            System.out.println("Ошибка открытия-закрытия файла: " + fileName + e);
-        } finally {
-            if (inFile != null) {
-                try {
-                    inFile.close();
-                } catch (IOException ignore) {
+        try{
+            File f1 = new File ("MyFile1.txt");
+            File f2 = new File ("MyFile2.txt");
+            BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(f1.getAbsolutePath()),"UTF8"));
+            PrintWriter bw = new PrintWriter(f2.getAbsolutePath(),"UTF8");
+            String s;
+            List <String> consonants= Arrays.asList("й","ц","к","н","г","ш","щ","з","х","ф","в","п","р","л","д","ж","ч","с","м","т","б");
+            int lineCounter=0;
+            while ((s=br.readLine())!=null) {
+                lineCounter+=1;
+                String resultString=""+lineCounter+": ";
+                String[] lineArray=s.toLowerCase().split(" ");
+                int count=0;
+                for (String word : lineArray) {
+                    String[] wordCharsArray=word.split("");
+                    if (consonants.contains(wordCharsArray[0]) ) {
+                        count+=1;
+                        resultString+=word+" ";
+                    }
+                }
+                if (count!=0) {
+                    bw.println(resultString+count);
                 }
             }
+            bw.flush();
+            bw.close();
+        }
+        catch (Exception e) {
         }
     }
 }
