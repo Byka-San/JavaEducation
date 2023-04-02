cd84638d53e67d60ae5dec697359680a5a085f5f
BykaSan <beasty348@yandex.ru>
2023-04-02 21:52:14 +0500

    Lr_11.1

--- src/Lr_11/example_05_01.java
+++ src/Lr_11/example_05_01.java
@@ -1,4 +1,15 @@
 package Lr_11;
 
 public class example_05_01 {
+    public static void main(String[] args) {
+        Node head = null;
+        for (int i = 0; i <= 9; i++){
+            head = new Node(i,head);
+        }
+        Node ref = head;
+        while (ref != null){
+            System.out.print(" " + ref.value);
+            ref = ref.next;
+        }
+    }
 }
